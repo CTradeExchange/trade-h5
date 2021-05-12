111fa2ac9a0c474a625e2ce565ccc5b66c1ef748
moore.hu@chixi88.com <qwert54321>
2021-05-12 10:33:43 +0800

    kyc

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -67,44 +67,6 @@ export default {
             commit('Update_loginLoading', true)
             return findCustomerInfo().then((res) => {
                 if (res.check()) {
-                    // 登录KYC,0未认证跳,需转到认证页面,1待审核,2审核通过,3审核不通过
-                    let msg, confirmButtonText
-
-                    if (Number(res.data.kycAuditStatus === 0)) {
-                        msg = '您还未进行KYC认证，点击去认证'
-                        confirmButtonText = '去认证'
-                        Dialog.alert({
-                            title: '提示',
-                            confirmButtonText: confirmButtonText,
-                            message: msg,
-                            theme: 'round-button',
-                        }).then(() => {
-                            router.push('/authentication')
-                        })
-                    } else if (Number(res.data.kycAuditStatus === 1)) {
-                        msg = '您的资料正在审核中，等耐心等待'
-                        confirmButtonText = '关闭'
-                        return Dialog.alert({
-                            title: '提示',
-                            confirmButtonText: confirmButtonText,
-                            message: msg,
-                            theme: 'round-button',
-                        }).then(() => {
-                            dispatch('_user/logout')
-                        })
-                    } else if (Number(res.data.kycAuditStatus === 3)) {
-                        msg = '您的资料审核失败'
-                        confirmButtonText = '重新提交'
-                        return Dialog.alert({
-                            title: '提示',
-                            confirmButtonText: confirmButtonText,
-                            message: msg,
-                            theme: 'round-button',
-                        }).then(() => {
-                            router.push('/authentication')
-                        })
-                    }
-
                     const data = res.data
                     commit('Update_kycState', res.data.kycAuditStatus)
                     commit('Update_customerInfo', res.data)
@@ -120,6 +82,36 @@ export default {
                     }
                 }
                 commit('Update_loginLoading', false)
+
+                // 登录KYC,0未认证跳,需转到认证页面,1待审核,2审核通过,3审核不通过
+                if (Number(res.data.kycAuditStatus === 0)) {
+                    Dialog.alert({
+                        title: '提示',
+                        confirmButtonText: '去认证',
+                        message: '您还未进行KYC认证，点击去认证',
+                        theme: 'round-button',
+                    }).then(() => {
+                        router.push('/authentication')
+                    })
+                } else if (Number(res.data.kycAuditStatus === 1)) {
+                    return Dialog.alert({
+                        title: '提示',
+                        confirmButtonText: '关闭',
+                        message: '您的资料正在审核中，等耐心等待',
+                        theme: 'round-button',
+                    }).then(() => {
+                        dispatch('_user/logout')
+                    })
+                } else if (Number(res.data.kycAuditStatus === 3)) {
+                    return Dialog.alert({
+                        title: '提示',
+                        confirmButtonText: '重新提交',
+                        message: '您的资料审核失败',
+                        theme: 'round-button',
+                    }).then(() => {
+                        router.push('/authentication')
+                    })
+                }
                 return res
             })
         },
