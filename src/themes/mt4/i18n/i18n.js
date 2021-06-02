6e7f1f936dafad7abbd132507a268cc63e19cfe5
amir.liu <amir.liu@chixi88.com>
2021-06-02 19:51:34 +0800

    多语言处理

---
+++ src/themes/mt4/i18n/i18n.js
@@ -0,0 +1,12 @@
+import { createI18n } from 'vue-i18n'
+import zhCN from './zh_CN.json'
+
+const i18n = createI18n({
+    // something vue-i18n options here ...
+    locale: 'zh-CN', // 设置语言
+    messages: {
+        'zh-CN': zhCN
+    }
+})
+
+export default i18n
