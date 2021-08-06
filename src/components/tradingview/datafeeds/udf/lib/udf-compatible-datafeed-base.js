7043316edfbeacaa5b601921dd09e99c4c407f48
damon <damon@email.com>
2021-08-06 14:48:55 +0800

    K线请求参数增加tradeType

--- src/components/tradingview/datafeeds/udf/lib/udf-compatible-datafeed-base.js
+++ src/components/tradingview/datafeeds/udf/lib/udf-compatible-datafeed-base.js
@@ -333,6 +333,7 @@ function normalizeInfo(info){
         description: info.description,
         symbolId: info.symbolId,
         pricescale:  Math.pow(10, info.digits),
+        tradeType: info.tradeType
     }
     return result
 }
