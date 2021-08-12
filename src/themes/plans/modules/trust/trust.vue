9674edf2ae2418c6e00eab3b50b7d7e78896df77
moore.hu@chixi88.com <qwert54321>
2021-08-12 19:44:36 +0800

    成交记录页面

--- src/themes/plans/modules/trust/trust.vue
+++ src/themes/plans/modules/trust/trust.vue
@@ -1,22 +1,27 @@
 <template>
     <div v-if='product && curProduct' class='trust-item'>
         <div class='t-header'>
-            <div class='product-name'>
-                <p>{{ product.symbolName }}</p>
-                <p class='time'>
-                    {{ formatTime(product.orderTime) }}
-                </p>
+            <div class='fl'>
+                <span class='name'>
+                    {{ product.symbolName }}
+                </span>
+                <span class='tag'>
+                    {{ product.expireType }}   {{ expireTypeMap['1'] }}
+                </span>
             </div>
-            <span class='direction'>
-                <span :class="Number(product.direction) === 1 ? 'riseColor' : 'fallColor'">
-                    {{ Number(product.direction) === 1 ? $t('trade.buy') :$t('trade.sell') }}
-                </span> {{ product.requestNum }}
-            </span>
-            <span class='fr'>
-                {{ product.expireType }}   {{ expireTypeMap['1'] }}
-            </span>
+
+            <div class='t-right'>
+                <van-button @click.stop='cancelOrder'>
+                    {{ $t('trade.cancelOrder') }}
+                </van-button>
+            </div>
         </div>
-        <Loading :show='loading' />
+        <div class='direction'>
+            <span :class="Number(product.direction) === 1 ? 'riseColor' : 'fallColor'">
+                {{ Number(product.direction) === 1 ? $t('trade.buy') :$t('trade.sell') }}
+            </span> {{ product.requestNum }}
+        </div>
+
         <div class='t-body'>
             <div class='t-left'>
                 <p class='tl-item'>
@@ -47,17 +52,24 @@
                     </label>
                     <span>{{ product.takeProfit }}</span>
                 </p>
+
+                <p class='tl-item'>
+                    <!-- <label for=''>
+                        {{ $t('trade.trustTime') }}
+                    </label> -->
+                    <span>{{ formatTime(product.orderTime,'YYYY/MM/DD hh:mm:ss') }}</span>
+                </p>
+
+                <p class='tl-item'>
+                    <!-- <label for=''>
+                        {{ $t('trade.trustId') }}
+                    </label> -->
+                    <span> #{{ product.id }}</span>
+                </p>
             </div>
-            <div class='t-right'>
-                <van-button @click.stop='cancelOrder'>
-                    {{ $t('trade.cancelOrder') }}
-                </van-button>
-            </div>
-        </div>
-        <div>
-            #{{ product.id }}
         </div>
     </div>
+    <Loading :show='loading' />
 </template>
 
 <script>
@@ -132,52 +144,43 @@ export default {
 <style lang="scss" scoped>
 @import '@/sass/mixin.scss';
 .trust-item {
-    padding: rem(30px);
+    margin: rem(20px);
+    padding: rem(20px);
     background-color: var(--contentColor);
     border-bottom: solid 1px var(--lineColor);
     border-radius: rem(8px);
     .t-header {
         display: flex;
+        align-items: center;
         justify-content: space-between;
-        //align-items: center;
-        margin-bottom: rem(30px);
-        .product-name {
+        margin-bottom: rem(20px);
+        .fl {
             margin-right: rem(32px);
             color: var(--color);
             font-size: rem(30px);
-            .time {
-                color: var(--placeholdColor);
-                font-size: rem(20px);
+            // .time {
+            //     color: var(--placeholdColor);
+            //     font-size: rem(20px);
+            // }
+            .name {
+                margin-right: rem(40px);
             }
         }
-        .direction {
-            font-size: rem(24px);
-        }
-    }
-    .t-body {
-        display: flex;
-        justify-content: space-between;
-        .t-left {
-            .tl-item {
-                margin-bottom: rem(10px);
-                color: var(--normalColor);
-                font-size: rem(24px);
-                text-align: left;
-                label {
-                    display: inline-block;
-                    width: rem(80px);
-                }
-                span {
-                    text-align: left;
-                }
-            }
+        .tag {
+            // width: 92px;
+            display: inline-block;
+            height: rem(35px);
+            padding: 0 rem(8px);
+            color: var(--minorColor);
+            font-size: rem(20px);
+            line-height: rem(35px);
+            text-align: center;
+            border: 1px solid var(--minorColor);
+            border-radius: rem(6px);
         }
         .t-right {
-            position: relative;
+            //position: relative;
             .van-button {
-                position: absolute;
-                right: 0;
-                bottom: rem(10px);
                 width: rem(124px);
                 height: rem(48px);
                 color: var(--primary);
@@ -186,6 +189,33 @@ export default {
                 background: var(--primaryAssistColor);
                 border-color: var(--primaryAssistColor);
                 border-radius: rem(6px);
+            }
+        }
+    }
+    .direction {
+        margin: rem(20px) 0;
+    }
+    .t-body {
+        display: flex;
+        justify-content: space-between;
+        .t-left {
+            display: flex;
+            flex-wrap: wrap;
+            .tl-item {
+                width: 50%;
+                margin-bottom: rem(10px);
+                color: var(--normalColor);
+                text-align: left;
+                label {
+                    display: inline-block;
+                    width: rem(100px);
+                    margin-right: rem(20px);
+                    font-size: rem(20px);
+                }
+                span {
+                    font-size: rem(20px);
+                    text-align: left;
+                }
             }
         }
     }
--- src/themes/plans/views/orderList/orderList.vue
+++ src/themes/plans/views/orderList/orderList.vue
@@ -14,20 +14,79 @@
                 </div>
             </van-tab>
             <van-tab :title='$t("trade.dealList")'>
-                <div class='order-wrap'>
-                    <van-dropdown-menu :overlay='true'>
+                <div class='filter-wrap'>
+                    <van-dropdown-menu :active-color='$style.primary' :overlay='true'>
                         <van-dropdown-item v-model='directionVal' :options='direction' />
-                        <van-dropdown-item v-model='productVal' :options='product' @open='open' />
+                        <van-dropdown-item ref='productDropdown' v-model='productVal' :options='product' @open='openProductSwitch' />
                         <van-dropdown-item v-model='positionTypeVal' :options='positionType' />
                         <van-dropdown-item v-model='timeVal' :options='timeList' />
                     </van-dropdown-menu>
+                </div>
+                <div class='list-wrap'>
+                    <!-- <div v-for='item in 20' :key='item' class='trust-item'>
+                        <div class='t-header'>
+                            <div class='fl'>
+                                <span class='name'>
+                                    欧元美元02
+                                </span>
+                            </div>
+                        </div><div class='direction'>
+                            限价开仓 /
+                            <span class='fallColor'>
+                                卖出
+                            </span> 0.01
+                        </div><div class='t-body'>
+                            <div class='t-left'>
+                                <p class='tl-item'>
+                                    <label for=''>
+                                        挂单价
+                                    </label><span>
+                                        49.00289
+                                    </span>
+                                </p><p class='tl-item'>
+                                    <label for=''>
+                                        成交价
+                                    </label><span class='grayColor'>
+                                        49.00089
+                                    </span>
+                                </p><p class='tl-item'>
+                                    <label for=''>
+                                        止损价
+                                    </label><span>
+                                        0
+                                    </span>
+                                </p>
+                                <p class='tl-item'>
+                                    <label for=''>
+                                        止盈
+                                    </label>
+                                    <span>
+                                        0
+                                    </span>
+                                </p>
+                                <p class='tl-item'>
+                                    <span>
+                                        2021/08/11 08:59:29
+                                    </span>
+                                </p><p class='tl-item'>
+                                    <span>
+                                        #1425441744087977984
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    </div> -->
                 </div>
             </van-tab>
         </van-tabs>
     </div>
 
     <!-- 侧边栏-切换产品 -->
-    <sidebarProduct v-model='switchProductVisible' @select='onSelectProduct' />
+    <sidebarProduct
+        v-model='switchProductVisible'
+        :trade-type='tradeType'
+        @select='onSelectProduct'
+    />
 </template>
 
 <script>
@@ -51,12 +110,16 @@ export default {
         const router = useRouter()
         const { tradeType } = route.query
 
+        const productDropdown = ref(null)
+
         const state = reactive({
             loading: false,
+            switchProductVisible: false,
             directionVal: 0,
             positionTypeVal: 0,
             timeVal: 0,
             productVal: 0,
+            curProduct: {},
             direction: [
                 { text: t('trade.direction'), value: 0 },
                 { text: t('trade.buy'), value: 1 },
@@ -101,7 +164,6 @@ export default {
 
         const onRefresh = () => {
             console.log('刷新')
-            loading = false
             queryPBOOrderPage()
         }
 
@@ -122,8 +184,9 @@ export default {
             { immediate: true }
         )
 
-        const open = (val) => {
-            debugger
+        const openProductSwitch = (val) => {
+            productDropdown.value.toggle(false)
+            state.switchProductVisible = true
         }
 
         const queryPBOOrderPage = () => {
@@ -136,6 +199,13 @@ export default {
             })
         }
 
+        // 产品选择品选择产品回调
+        const onSelectProduct = (p) => {
+            state.curProduct = p
+            state.product[0].text = p.symbolName
+            state.switchProductVisible = false
+        }
+
         queryPBOOrderPage()
 
         return {
@@ -143,8 +213,11 @@ export default {
             pendingList,
             loading,
             tradeName,
-            open,
+            onSelectProduct,
+            openProductSwitch,
             onRefresh,
+            tradeType,
+            productDropdown,
              ...toRefs(state),
 
         }
@@ -183,6 +256,63 @@ export default {
         }
         .van-dropdown-menu__item {
             background-color: var(--contentColor);
+        }
+        .van-dropdown-menu {
+            background-color: var(--contentColor);
+        }
+        .list-wrap {
+            .trust-item {
+                margin: rem(20px);
+                padding: rem(20px);
+                background-color: var(--contentColor);
+                border-bottom: solid 1px var(--lineColor);
+                border-radius: rem(8px);
+                .t-header {
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                    margin-bottom: rem(20px);
+                    .fl {
+                        margin-right: rem(32px);
+                        color: var(--color);
+                        font-size: rem(30px);
+                        // .time {
+                        //     color: var(--placeholdColor);
+                        //     font-size: rem(20px);
+                        // }
+                        .name {
+                            margin-right: rem(40px);
+                        }
+                    }
+                }
+                .direction {
+                    margin: rem(20px) 0;
+                }
+                .t-body {
+                    display: flex;
+                    justify-content: space-between;
+                    .t-left {
+                        display: flex;
+                        flex-wrap: wrap;
+                        .tl-item {
+                            width: 50%;
+                            margin-bottom: rem(10px);
+                            color: var(--normalColor);
+                            text-align: left;
+                            label {
+                                display: inline-block;
+                                width: rem(100px);
+                                margin-right: rem(20px);
+                                font-size: rem(20px);
+                            }
+                            span {
+                                font-size: rem(20px);
+                                text-align: left;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
     .trust-item {
