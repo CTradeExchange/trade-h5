c1457402f5c60522ea34e797dbc62d318182d016
ecn-pipy <pipy.pi@shaomaimedia.com>
2021-08-20 10:21:25 +0800

    修改首页信息流

--- src/api/information.js
+++ src/api/information.js
@@ -1,27 +1,37 @@
 import request from '@/utils/request_information'
 
-export function newsListByType (data) {
+const baseLangURL = {
+    cn: 'https://prodcn.displore.com.cn', // 中文
+    en: 'https://proden.displore.com.cn', // 英文
+    in: 'https://prodin.displore.com.cn', // 印尼
+}
+
+export function newsListByType (data, lang) {
+    const baseURL = baseLangURL[lang] || baseLangURL.cn
     return request({
-        url: '/api/article/newsListByType',
+        url: baseURL + '/api/article/newsListByType',
         data
     })
 }
-export function newsListByTypeByPage (data) {
+export function newsListByTypeByPage (data, lang) {
+    const baseURL = baseLangURL[lang] || baseLangURL.cn
     return request({
-        url: '/api/article/newsListByTypeByPage',
+        url: baseURL + '/api/article/newsListByTypeByPage',
 
         data
     })
 }
-export function canlendarListByDate (data) {
+export function canlendarListByDate (data, lang) {
+    const baseURL = baseLangURL[lang] || baseLangURL.cn
     return request({
-        url: '/api/canlendar/listByDate',
+        url: baseURL + '/api/canlendar/listByDate',
         data
     })
 }
-export function articleDetail (data) {
+export function articleDetail (data, lang) {
+    const baseURL = baseLangURL[lang] || baseLangURL.cn
     return request({
-        url: '/api/article/detail',
+        url: baseURL + '/api/article/detail',
         data
     })
 }
