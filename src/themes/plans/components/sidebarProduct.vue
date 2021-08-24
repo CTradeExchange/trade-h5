366988acc2aacf2c8e04f0bdd669a017735f5308
damon <damon@email.com>
2021-08-24 10:26:17 +0800

    行情和产品切换侧边栏，屏蔽tradeType === '5'的玩法

--- src/themes/plans/components/sidebarProduct.vue
+++ src/themes/plans/components/sidebarProduct.vue
@@ -1,7 +1,14 @@
 <template>
     <van-popup v-model:show='show' position='left' :style="{ height: '100%' }" teleport='body' @open='onOpen'>
         <div class='sidebarProduct'>
-            <plansType v-if='!hideTradeType' ref='plansTypeRef' class='plansType' :value='tradeType' @change='handleTradeType' />
+            <plansType
+                v-if='!hideTradeType'
+                ref='plansTypeRef'
+                class='plansType'
+                :list='plansList'
+                :value='tradeType'
+                @change='handleTradeType'
+            />
             <search
                 :class='{ margin: hideTradeType }'
                 :trade-type='tradeType'
@@ -73,7 +80,7 @@ export default {
         }
 
         // 玩法列表
-        const plansList = computed(() => store.state._base.plans)
+        const plansList = computed(() => store.state._base.plans.filter(e => e.tradeType !== '5'))
         // 默认玩法类型
         const InitialTradeType = computed(() => String(props.defaultTradeType) || unref(plansList)[0].id)
         // 1.玩法类型
--- src/themes/plans/views/quote/quote.vue
+++ src/themes/plans/views/quote/quote.vue
@@ -14,7 +14,7 @@
                 @change='tabChange'
                 @tabClick='tabClick'
             />
-            <van-icon name="search" @click="openSearch"/>
+            <van-icon name='search' @click='openSearch' />
         </div>
         <div class='titleBar van-hairline--bottom'>
             <span class='item'>
@@ -52,7 +52,7 @@ export default {
         const router = useRouter()
         const productListEl = ref(null)
         // 玩法列表
-        const plansList = computed(() => store.state._base.plans)
+        const plansList = computed(() => store.state._base.plans.filter(e => e.tradeType !== '5'))
 
         // 1.玩法类型
         const tradeType = ref(plansList.value[0].id)
@@ -86,7 +86,7 @@ export default {
         const tabClick = (i) => {}
 
         const showSidebar = ref(false)
-        const openSearch = ()=>{
+        const openSearch = () => {
             router.push('/productSearch')
         }
         return {
