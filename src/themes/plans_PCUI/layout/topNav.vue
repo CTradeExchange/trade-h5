24270a95099bcd2a8a4ad1e8142f7fcd7903371c
curry <curry034@uuubee.com>
2021-11-01 16:32:45 +0800

    消息列表

--- src/themes/plans_PCUI/layout/topNav.vue
+++ src/themes/plans_PCUI/layout/topNav.vue
@@ -63,12 +63,65 @@
                     <i class='icon icon_zichan' :title="$t('header.assets')"></i>
                 </div>
                 <div class='item'>
-                    <i class='icon icon_xiaoxizhongxin1' :title="$t('header.information')"></i>
+                     <el-dropdown trigger="click">
+                        <i class='icon icon_xiaoxizhongxin1' :title="$t('header.information')"></i>
+                        <template #dropdown>
+                                <div class='information_box' id="information_head">
+                                <div class="information_head" >
+                                    <div class="current_type" @click="dropTypeVisible = !dropTypeVisible">
+                                        <span>{{informationType}}</span><i class='icon el-icon-caret-bottom'></i>
+                                    </div>
+                                <!--
+                                    <van-dropdown-menu get-container="#information_head">
+                                        <van-dropdown-item v-model='type' :options='options' @change='changeType' />
+                                    </van-dropdown-menu>
+                                -->
+                                </div>
+                                <div class="type_list" v-if="dropTypeVisible">
+                                    <ul>
+                                        <li v-for="item in options" :class="{activeLi:type==item.value}" @click="changeType(item)">{{item.text}}</li>
+                                    </ul>
+                                </div>
+                                <Loading :show='pageLoading' />
+                                <div class='msg-list'>
+                                    <div v-if='list.length === 0'>
+                                        <van-empty :description='$t("common.noData")' image='/images/empty.png' />
+                                    </div>
+                                    <van-pull-refresh v-else v-model='loading' @refresh='onRefresh'>
+                                        <van-list
+                                            v-model:error='isError'
+                                            v-model:loading='loading'
+                                            :error-text='errorTip'
+                                            :finished='finished'
+                                            :finished-text='$t("common.noMore")'
+                                            @load='onLoad'
+                                        >
+                                            <div v-for='(item,index) in list' :key='index' class='msg-item'>
+                                                <p class='msg-title'>
+                                                    {{ item.title === 'null'? '': item.title }}
+                                                </p>
+                                                <p class='msg-content'>
+                                                    {{ computeHtmlTime(item.content) }}
+                                                </p>
+                                                <p class='msg-time'>
+                                                    {{ formatTime(item.createTime) }}
+                                                </p>
+                                            </div>
+                                        </van-list>
+                                    </van-pull-refresh>
+                                </div>
+
+
+
+                            </div>
+                        </template>
+                    </el-dropdown>
                 </div>
                 <div class='item'>
                     <i class='icon icon_shezhi' :title="$t('header.set')"></i>
                 </div>
                 <div class='line'></div>
+                
             </div>
             <!-- 操作功能 -->
             <div class='handle-feature'>
@@ -116,17 +169,150 @@
 </template>
 
 <script>
-import { computed } from 'vue'
+import { onBeforeMount, computed, reactive, toRefs, onUnmounted } from 'vue'
+import { queryPlatFormMessageLogList } from '@/api/user'
 import { useStore } from 'vuex'
-
+import Top from '@/components/top'
+import { isEmpty } from '@/utils/util'
+import { useI18n } from 'vue-i18n'
 export default {
     setup () {
         const store = useStore()
-        // 玩法列表
-        const plansList = computed(() => store.state._base.plans)
+        const { t } = useI18n({ useScope: 'global' })
+        const state = reactive({
+            list: [],
+            loading: false,
+            finished: false,
+            pageLoading: false,
+            current: 1,
+            type: '',
+            errorTip: '',
+            rightAction: { title: 444 },
+            options: [
+                {
+                    'text': t('msg.all'),
+                    'value': ''
+                },
+                {
+                    'text': t('msg.accountMsg'),
+                    'value': 'USER_MESSAGE'
+                },
+                {
+                    'text': t('msg.assetsMsg'),
+                    'value': 'CASH_MESSAGE'
+                },
+                {
+                    'text': t('msg.tradeMsg'),
+                    'value': 'TRADE_MESSAGE'
+                }
+            ],
+            informationType:"全部消息",
+            dropTypeVisible:false,
+        })
+        const isError = computed(() => !!state.isError)
+
+        // 获取账户信息
+        const customInfo = computed(() => store.state._user.customerInfo)
+
+        const changeType  = (item) => {
+            console.log(item)
+            if(state.type==item.value){
+                return;
+            }
+            state.type = item.value
+            state.informationType = item.text
+            state.current = 1
+            state.finished = false
+            state.list = []
+            state.dropTypeVisible = !state.dropTypeVisible
+            getMsgList()
+        }
+
+        const getMsgList = () => {
+            state.pageLoading = true
+            state.errorTip = ''
+            queryPlatFormMessageLogList({
+                current: state.current,
+                parentType: state.type,
+            }).then(res => {
+                state.loading = false
+                state.pageLoading = false
+                if (res.check()) {
+                    if (res.data.records && res.data.records.length > 0) {
+                        state.list = state.list.concat(res.data.records)
+                    }
+
+                    // 数据全部加载完成
+                    if (res.data.size * res.data.current >= res.data.total) {
+                        state.finished = true
+                    }
+                }
+            }).catch(err => {
+                state.errorTip = t('c.loadError')
+                state.pageLoading = false
+            })
+        }
+
+        const computeHtmlTime = (content) => {
+            try {
+                const reg = /<?time[^>]*>[^<]*<\/time>/gi
+                const tag = content.match(reg)
+                let returnVal
+                if (!isEmpty(tag) && tag.length > 0) {
+                    tag.forEach(item => {
+                        returnVal = content.replace(reg, function (matchStr) {
+                            const time = matchStr.toString().replace(/<\/?time>/g, '')
+                            return window.dayjs(Number(time)).format('YYYY-MM-DD HH:mm:ss')
+                        })
+                    })
+                    return returnVal
+                } else {
+                    return content
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        // 获取到顶部消息通知，同时刷新消息列表
+        const gotMsg = () => {
+            onRefresh()
+        }
+        document.body.addEventListener('GotMsg_notice', gotMsg, false)
+        onBeforeMount(() => {
+            getMsgList()
+        })
+        onUnmounted(() => {
+            document.body.removeEventListener('GotMsg_notice', gotMsg)
+        })
+
+        // 上拉刷新
+        const onRefresh = () => {
+            state.current = 1
+            state.finished = false
+            state.list = []
+            getMsgList()
+        }
+        // 底部加载更多
+        const onLoad = () => {
+            state.current++
+            getMsgList()
+        }
+
+        const formatTime = (val) => {
+            return window.dayjs(val).format('YYYY-MM-DD HH:mm:ss')
+        }
 
         return {
-            plansList
+            getMsgList,
+            isError,
+            customInfo,
+            formatTime,
+            onRefresh,
+            onLoad,
+            changeType,
+            computeHtmlTime,
+            ...toRefs(state)
         }
     }
 }
@@ -242,6 +428,7 @@ export default {
                     color: #D6DAE1;
                     cursor: pointer;
                 }
+                
             }
             .line {
                 width: 1px;
@@ -268,7 +455,77 @@ export default {
         }
     }
 }
-
+.information_box{
+    width:400px;
+    height:600px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+    background-color:rgba(0, 0, 0, 0.2);
+    .information_head{
+        width:100%;
+        height:48px;
+        background-color:#ffffff;
+        text-align:center;
+        box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+        .current_type{
+            font-size: 16px;
+            line-height: 48px;
+            cursor:pointer;
+            i{
+                margin-left:4px;
+                color:#dcdfe6;
+                cursor:pointer;
+            }
+        }
+    }
+    .type_list{
+        ul{
+            width:100%;
+            li{
+                width:100%;
+                height:40px;
+                line-height: 44px;
+                font-size: 14px;
+                text-align:center;
+                background-color:#ffffff;
+                cursor:pointer;
+            }
+        }
+        .activeLi{
+            color:#ee0a24;
+        }
+    }
+    .msg-list {
+        flex: 1;
+        height: 552px;
+        overflow: auto;
+        background-color: var(--bgColor);
+        .msg-item {
+            margin: 7px;
+            padding: 10px;
+            background-color: var(--contentColor);
+            border-top: solid 10px var(--bgColor);
+            .msg-title {
+                color: var(--color);
+                font-weight: bold;
+                font-size: 14px;
+                line-height: 30px;
+            }
+            .msg-content {
+                color: var(--color);
+                font-weight: 500;
+                font-size: 12px;
+                line-height: 20px;
+            }
+            .msg-time {
+                color: var(--minorColor);
+                font-weight: 400;
+                font-size: 12px;
+                line-height: 30px;
+            }
+        }
+    }
+}
 .download-dialog {
     display: flex;
     flex-direction: column;
