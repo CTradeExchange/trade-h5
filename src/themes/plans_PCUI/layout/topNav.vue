27f720b5e43ca8d03022adfc4d84b8c58b49d1ef
amir.liu <amir.liu@chixi88.com>
2021-11-04 13:40:03 +0800

    白天黑夜模式切换功能

--- src/themes/plans_PCUI/layout/topNav.vue
+++ src/themes/plans_PCUI/layout/topNav.vue
@@ -110,8 +110,12 @@
                         <i class='icon icon_zhuanhuanchengbaitian' :title="$t('header.theme')"></i>
                         <template #dropdown>
                             <el-dropdown-menu>
-                                <el-dropdown-item>白天</el-dropdown-item>
-                                <el-dropdown-item>黑夜</el-dropdown-item>
+                                <el-dropdown-item @click="changeTheme('light')">
+                                    白天
+                                </el-dropdown-item>
+                                <el-dropdown-item @click="changeTheme('night')">
+                                    黑夜
+                                </el-dropdown-item>
                             </el-dropdown-menu>
                         </template>
                     </el-dropdown>
@@ -129,6 +133,7 @@ import { useI18n } from 'vue-i18n'
 import { useRoute, useRouter } from 'vue-router'
 import SettingIcon from './components/settingIcon'
 import Msg from './components/msg'
+import Colors, { setRootVariable } from '@planspc/colorVariables'
 
 export default {
     components: {
@@ -164,6 +169,24 @@ export default {
         // 路由跳转
         const handRoutTo = (path) => router.push(route.path + path)
 
+        // 切换白天黑夜模式主题
+        const changeTheme = theme => {
+            // 设置全局变量
+            store.commit('Update_invertColor', theme)
+            setRootVariable(theme)
+            state.colorVisible = false
+
+            document.body.style.setProperty('--color', Colors[theme].color)
+            document.body.style.setProperty('--contentColor', Colors[theme].contentColor)
+            document.body.style.setProperty('--primaryAssistColor', Colors[theme].primaryAssistColor)
+            document.body.style.setProperty('--bgColor', Colors[theme].bgColor)
+            document.body.style.setProperty('--normalColor', Colors[theme].normalColor)
+            document.body.style.setProperty('--minorColor', Colors[theme].minorColor)
+            document.body.style.setProperty('--lineColor', Colors[theme].lineColor)
+            document.body.style.setProperty('--assistColor', Colors[theme].assistColor)
+            document.body.style.setProperty('--placeholdColor', Colors[theme].placeholdColor)
+        }
+
         return {
             plansList,
             userAccountType,
@@ -171,6 +194,7 @@ export default {
             handRoutTo,
             customInfo,
             formatTime,
+            changeTheme,
             ...toRefs(state)
         }
     }
