4e15be94d9fdeb93797a447b077361b295894ebb
moore.hu@chixi88.com <qwert54321>
2021-11-08 11:44:00 +0800

    切换主题颜色更换图标

--- src/themes/plans_PCUI/layout/components/themeIcon.vue
+++ src/themes/plans_PCUI/layout/components/themeIcon.vue
@@ -1,48 +1,47 @@
 <template>
-    <el-dropdown>
-        <i class='icon icon_zhuanhuanchengbaitian' :title="$t('header.theme')"></i>
-        <template #dropdown>
-            <el-dropdown-menu>
-                <el-dropdown-item @click="changeTheme('light')">
-                    {{ $t('theme.day') }}
-                </el-dropdown-item>
-                <el-dropdown-item @click="changeTheme('night')">
-                    {{ $t('theme.night') }}
-                </el-dropdown-item>
-            </el-dropdown-menu>
-        </template>
-    </el-dropdown>
+    <i
+        class='icon'
+        :class="theme === 'light' ? 'icon_zhuanhuanchengbaitian' : 'icon_zhuanhuanchengheiye'"
+        :title="$t('header.theme')"
+        @click='changeTheme'
+    ></i>
 </template>
 
 <script>
 import { reactive, toRefs } from 'vue'
 import { useStore } from 'vuex'
 import Colors, { setRootVariable } from '@planspc/colorVariables'
+import { localGet } from '@/utils/util'
 export default {
     setup () {
         const store = useStore()
+        const state = reactive({
+            theme: localGet('invertColor')
+        })
 
         // 切换白天黑夜模式主题
-        const changeTheme = theme => {
+        const changeTheme = () => {
+            state.theme = localGet('invertColor') === 'light' ? 'night' : 'light'
             // 设置全局变量
-            store.commit('Update_invertColor', theme)
-            setRootVariable(theme)
+            store.commit('Update_invertColor', state.theme)
+            setRootVariable(state.theme)
+            document.body.style.setProperty('--color', Colors[state.theme].color)
+            document.body.style.setProperty('--contentColor', Colors[state.theme].contentColor)
+            document.body.style.setProperty('--primaryAssistColor', Colors[state.theme].primaryAssistColor)
+            document.body.style.setProperty('--bgColor', Colors[state.theme].bgColor)
+            document.body.style.setProperty('--normalColor', Colors[state.theme].normalColor)
+            document.body.style.setProperty('--minorColor', Colors[state.theme].minorColor)
+            document.body.style.setProperty('--lineColor', Colors[state.theme].lineColor)
+            document.body.style.setProperty('--assistColor', Colors[state.theme].assistColor)
+            document.body.style.setProperty('--placeholdColor', Colors[state.theme].placeholdColor)
 
-            document.body.style.setProperty('--color', Colors[theme].color)
-            document.body.style.setProperty('--contentColor', Colors[theme].contentColor)
-            document.body.style.setProperty('--primaryAssistColor', Colors[theme].primaryAssistColor)
-            document.body.style.setProperty('--bgColor', Colors[theme].bgColor)
-            document.body.style.setProperty('--normalColor', Colors[theme].normalColor)
-            document.body.style.setProperty('--minorColor', Colors[theme].minorColor)
-            document.body.style.setProperty('--lineColor', Colors[theme].lineColor)
-            document.body.style.setProperty('--assistColor', Colors[theme].assistColor)
-            document.body.style.setProperty('--placeholdColor', Colors[theme].placeholdColor)
-
-            const event = new CustomEvent('Launch_theme', { detail: theme })
+            const event = new CustomEvent('Launch_theme', { detail: state.theme })
             document.body.dispatchEvent(event)
         }
+
         return {
-            changeTheme
+            changeTheme,
+            ...toRefs(state)
         }
     }
 }
