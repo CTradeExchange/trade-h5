54b40ad37150f3dc390d4e689305a3d08ae76ba7
moore.hu@chixi88.com <qwert54321>
2021-11-10 15:17:59 +0800

    恢复文件

---
+++ src/themes/plans_PCUI/views/order/order.vue
@@ -0,0 +1,192 @@
+<template>
+    <div class='page-wrap'>
+        <router-view />
+        <div class='content-top' :style="'height: '+ contentHeight">
+            <div class='quote-wrap'>
+                <!-- 产品列表/搜索 -->
+                <sidebarProduct />
+            </div>
+            <div class='middle-wrap'>
+                <div class='chart-content'>
+                    <!-- 图表 -->
+                    <chart />
+                </div>
+                <div class='trade-content' :style="'height: '+ tradeContentHeight">
+                    <!-- 交易下单 -->
+                    <trade />
+                </div>
+            </div>
+            <div v-if='Number(product.tradeType) === 5' class='right-wrap'>
+                <!-- 盘口报价 -->
+                <div class='handicap-content'>
+                    <handicap :product='product' />
+                </div>
+                <!-- 实时成交记录 -->
+                <div class='deal-content'>
+                    <dealList :symbol-id='product?.symbolId' />
+                </div>
+            </div>
+        </div>
+        <div class='orders-wrap'>
+            <!-- 委托/记录/资产 -->
+            <userRecord />
+        </div>
+        <van-sticky class='assetsSticky' :offset-bottom='10' position='bottom'>
+            <!-- 资产 -->
+            <assetsModule />
+        </van-sticky>
+    </div>
+</template>
+
+<script>
+import { reactive, toRefs, computed } from 'vue'
+import chart from './pages/chart.vue'
+import { useRouter, useRoute } from 'vue-router'
+import handicap from './pages/handicap.vue'
+import dealList from './pages/dealList.vue'
+import trade from './pages/trade.vue'
+import sidebarProduct from './pages/sidebarProduct'
+import assetsModule from './pages/assets.vue'
+import { isEmpty } from '@/utils/util'
+import { useStore } from 'vuex'
+import userRecord from './pages/userRecord'
+
+export default {
+    components: {
+        chart,
+        handicap,
+        dealList,
+        trade,
+        sidebarProduct,
+        assetsModule,
+        userRecord
+    },
+    setup () {
+        const store = useStore()
+        const route = useRoute()
+        const router = useRouter()
+        const { tradeType, symbolId } = route.query
+        store.commit('_quote/Update_productActivedID', `${symbolId}_${tradeType}`)
+        const product = computed(() => store.getters.productActived)
+        if (isEmpty(product.value)) {
+            router.push('/')
+        }
+        // 获取产品详情
+        store.dispatch('_quote/querySymbolInfo', { 'symbolId': product.value.symbolId, 'tradeType': product.value.tradeType })
+
+        const tradeContentHeight = computed(() => {
+            if (Number(product.value?.tradeType) === 5) {
+                return '265px'
+            } else if (Number(product.value?.tradeType) === 3) {
+                return '340px'
+            } else {
+                return '430px'
+            }
+        })
+
+        const contentHeight = computed(() => {
+            if (Number(product.value?.tradeType) === 3) {
+                return '785px'
+            } else if (Number(product.value?.tradeType) === 5) {
+                return '710px'
+            } else {
+                return '874px'
+            }
+        })
+        return {
+            product,
+            tradeType,
+            symbolId,
+            tradeContentHeight,
+            contentHeight
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/sass/mixin.scss";
+
+.page-wrap {
+    font-size: 14px;
+    padding:  8px;
+    background: var(--bgColor);
+
+    .content-top {
+        max-height: 874px;
+        display: flex;
+        justify-content: space-between;
+
+        >div {
+            margin-right: 8px;
+            border-radius: 10px;
+            background: var(--contentColor);
+            &:last-child{
+                margin-right: 0;
+            }
+        }
+
+        .quote-wrap {
+            width: 360px;
+            // height: 709px;
+            display: flex;
+            flex-direction: row;
+        }
+
+        .middle-wrap {
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            background: var(--bgColor);
+            >div{
+                 border-radius: 10px;
+                 background: var(--contentColor);
+            }
+            .chart-content{
+                margin-bottom: 8px;
+                height: 436px;
+                overflow: hidden;
+
+            }
+            .trade-content{
+                position: relative;
+                padding: 5px 16px 20px 16px;
+            }
+        }
+
+        .right-wrap {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            width: 280px;
+            background: var(--bgColor);
+            >div{
+                border-radius: 10px;
+                background: var(--contentColor);
+            }
+            .handicap-content{
+                padding: 10px 0 8px 16px;
+                height: 368px;
+
+            }
+            .deal-content{
+                padding: 10px 16px 10px 16px;
+                height: 333px;
+                overflow:hidden
+            }
+        }
+    }
+    .orders-wrap{
+        margin-top: 8px;
+        min-height: 300px;
+        background: var(--contentColor);
+    }
+    .assetsSticky{
+        :deep(.van-sticky--fixed){
+            box-shadow: 0 0 10px rgba(0,0,0,.15);
+            background-color: var(--bgColor);
+        }
+    }
+}
+</style>
