d5f6d661ab418a445e3f2a5af8afe86c0db94d1c
amir.liu <amir.liu@chixi88.com>
2021-11-25 20:19:52 +0800

    增加端口检测

--- build_folder/cats-upload-admin/cats-upload-admin/cats-upload-admin.php
+++ build_folder/cats-upload-admin/cats-upload-admin/cats-upload-admin.php
@@ -17,7 +17,7 @@ function cats_start_upload_admin_install() {
     $sllHttp = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
     $p_data['type'] = 'H5';
     $p_data['folder'] = plugin_dir_path(__FILE__ ) . 'dist';
-    uploadAdminHttpPost($sllHttp . $_SERVER['SERVER_NAME'] . '/wp-json/wp/v2/uploadAdminEditorPluginApi',$p_data);
+    uploadAdminHttpPost($sllHttp . $_SERVER['SERVER_NAME'] . ':' .$_SERVER["SERVER_PORT"] . '/wp-json/wp/v2/uploadAdminEditorPluginApi',$p_data);
 }
 
 /**
