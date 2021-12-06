579c00c16b7196aee54829fe5e6d1613873c6d49
amir.liu <amir.liu@chixi88.com>
2021-12-06 15:52:47 +0800

    主题基础模块配置修改 主题的ini文件新增pages页面配置

--- src/themes/plans_PCUI/wp_preview/pageBaseConfig.js
+++ src/themes/plans_PCUI/wp_preview/pageBaseConfig.js
@@ -1,10 +1,10 @@
-// import HomeFullBannerConfig from '@planspc/modules/fullBanner/config.js'
+import HomeFullBannerConfig from '@planspc/modules/fullBanner/config.js'
 // import navConfig from '@index/preview/modules/nav/config.js'
 // import quoteListConfig from '@index/preview/modules/quoteList/config.js'
 // import selfSymbolConfig from '@index/preview/modules/selfSymbol/config.js'
 // import mineTopConfig from '@index/preview/modules/mineTop/config.js'
 export const Home = [
-    // HomeFullBannerConfig
+    HomeFullBannerConfig
 ]
 export const Nav = [
     // navConfig
