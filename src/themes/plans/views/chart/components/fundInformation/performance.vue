f663cbcca4b2dd2f27a323579cb8679a21054b5b
amir.liu <amir.liu@chixi88.com>
2022-01-17 17:35:38 +0800

    基金产品信息的市场表现屏蔽小时的折线图

--- src/themes/plans/views/chart/components/fundInformation/performance.vue
+++ src/themes/plans/views/chart/components/fundInformation/performance.vue
@@ -6,18 +6,18 @@
         <div class='chartModule'>
             <div id='marketPriceJZ_DOM' ref='marketPriceJZ_DOM' class='chartLine'></div>
         </div>
-        <div class='chartModule'>
+        <!-- <div class='chartModule'>
             <div id='marketPriceJZ_DOM1' ref='marketPriceJZ_DOM1' class='chartLine'></div>
-        </div>
+        </div> -->
         <h4 class='title spaceLine'>
             {{ $t('fundInfo.fundChart2') }}
         </h4>
         <div class='chartModule'>
             <div id='marketAppearance_DOM' ref='marketAppearance_DOM' class='chartLine'></div>
         </div>
-        <div class='chartModule'>
+        <!-- <div class='chartModule'>
             <div id='marketAppearance_DOM1' ref='marketAppearance_DOM1' class='chartLine'></div>
-        </div>
+        </div> -->
         <BottomTip />
     </div>
 </template>
@@ -70,39 +70,41 @@ onMounted(async () => {
             marketPriceJZ_DOM.value.style.display = 'none'
         }
     })
-    getMarketPerformanceData1().then(res => {
-        const [data1, data2] = res
-        const xData = []
-        const yData = []
-        const xDataSource = data1 || data2 || ['', []]
-        xDataSource[1].forEach(el => {
-            xData.push(el.time.slice(0, 16))
-        })
-        if (data1?.length) {
-            yData.push({
-                name: data1[0],
-                type: 'line',
-                stack: 'Total',
-                showSymbol: false,
-                data: data1[1].map(el => el.value)
-            })
-        }
 
-        if (data2?.length) {
-            yData.push({
-                name: data2[0],
-                type: 'line',
-                stack: 'Total',
-                showSymbol: false,
-                data: data2[1].map(el => el.value)
-            })
-        }
-        if (yData.length) {
-            newChart(marketPriceJZ_DOM1.value, [xData, yData])
-        } else {
-            marketPriceJZ_DOM1.value.style.display = 'none'
-        }
-    })
+    // 市场价格 vs 基金净值  横坐标以小时归类
+    // getMarketPerformanceData1().then(res => {
+    //     const [data1, data2] = res
+    //     const xData = []
+    //     const yData = []
+    //     const xDataSource = data1 || data2 || ['', []]
+    //     xDataSource[1].forEach(el => {
+    //         xData.push(el.time.slice(0, 16))
+    //     })
+    //     if (data1?.length) {
+    //         yData.push({
+    //             name: data1[0],
+    //             type: 'line',
+    //             stack: 'Total',
+    //             showSymbol: false,
+    //             data: data1[1].map(el => el.value)
+    //         })
+    //     }
+
+    //     if (data2?.length) {
+    //         yData.push({
+    //             name: data2[0],
+    //             type: 'line',
+    //             stack: 'Total',
+    //             showSymbol: false,
+    //             data: data2[1].map(el => el.value)
+    //         })
+    //     }
+    //     if (yData.length) {
+    //         newChart(marketPriceJZ_DOM1.value, [xData, yData])
+    //     } else {
+    //         marketPriceJZ_DOM1.value.style.display = 'none'
+    //     }
+    // })
 
     // 市场表现
     getMarketPerformanceQuoteChange().then(res => {
@@ -140,40 +142,41 @@ onMounted(async () => {
         }
     })
 
-    getMarketPerformanceQuoteChange1().then(res => {
-        const [data1, data2] = res
-        const xData = []
-        const yData = []
-        const xDataSource = data1 || data2 || ['', []]
-        xDataSource[1].forEach(el => {
-            xData.push(el.time.slice(0, 16))
-        })
-        if (data1?.length) {
-            yData.push({
-                name: data1[0],
-                type: 'line',
-                stack: 'Total',
-                showSymbol: false,
-                data: data1[1].map(el => el.value)
-            })
-        }
-        if (data2?.length) {
-            yData.push({
-                name: data2[0],
-                type: 'line',
-                stack: 'Total',
-                showSymbol: false,
-                data: data2[1].map(el => el.value)
-            })
-        }
-        if (yData.length) {
-            newChart(marketAppearance_DOM1.value, [xData, yData], {
-                'yAxis.ext': '%'
-            })
-        } else {
-            marketAppearance_DOM1.value.style.display = 'none'
-        }
-    })
+    // 市场表现  横坐标以小时归类
+    // getMarketPerformanceQuoteChange1().then(res => {
+    //     const [data1, data2] = res
+    //     const xData = []
+    //     const yData = []
+    //     const xDataSource = data1 || data2 || ['', []]
+    //     xDataSource[1].forEach(el => {
+    //         xData.push(el.time.slice(0, 16))
+    //     })
+    //     if (data1?.length) {
+    //         yData.push({
+    //             name: data1[0],
+    //             type: 'line',
+    //             stack: 'Total',
+    //             showSymbol: false,
+    //             data: data1[1].map(el => el.value)
+    //         })
+    //     }
+    //     if (data2?.length) {
+    //         yData.push({
+    //             name: data2[0],
+    //             type: 'line',
+    //             stack: 'Total',
+    //             showSymbol: false,
+    //             data: data2[1].map(el => el.value)
+    //         })
+    //     }
+    //     if (yData.length) {
+    //         newChart(marketAppearance_DOM1.value, [xData, yData], {
+    //             'yAxis.ext': '%'
+    //         })
+    //     } else {
+    //         marketAppearance_DOM1.value.style.display = 'none'
+    //     }
+    // })
 })
 </script>
 
