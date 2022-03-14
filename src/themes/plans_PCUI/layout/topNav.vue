80949928ab1ceb01fbd9467c4f84f5f87ca8712b
Moore.h <you@example.com>
2022-03-14 14:33:13 +0800

    KYC入口判断

--- src/themes/plans_PCUI/layout/topNav.vue
+++ src/themes/plans_PCUI/layout/topNav.vue
@@ -80,7 +80,7 @@
                         <i class='icon icon_gerenxinxi' :title="$t('cRoute.personal')"></i>
                         <template #dropdown>
                             <el-dropdown-menu>
-                                <el-dropdown-item v-if='Number(customerInfo.companyKycStatus)===1 && Number(customerInfo.openAccountType) === 1' @click="$router.push('/businessKYC')">
+                                <el-dropdown-item v-if='Number(customerInfo.openAccountType) === 1' @click="$router.push('/businessKYC')">
                                     企业KYC认证
                                 </el-dropdown-item>
                                 <el-dropdown-item v-if='Number(customerInfo.companyKycStatus)===1 && Number(customerInfo.openAccountType) === 0' @click="handRoutTo('/authentication')">
