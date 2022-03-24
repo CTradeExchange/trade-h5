e600277d970cd83f6e36d5b1258cc43a6ce8ad00
Moore.h <you@example.com>
2022-03-24 21:19:40 +0800

    首页修改

--- src/themes/plans/layout/layoutTop.vue
+++ src/themes/plans/layout/layoutTop.vue
@@ -4,13 +4,15 @@
             <img alt='' class='logo' src='/images/vitamin_logo.png' srcset='' />
             <div class='text'>
                 <p>Vitatoken APP</p>
-                <p>Secure, fast and elegant.</p>
+                <p>{{ $t('vitaHome.app') }}</p>
             </div>
         </div>
 
         <div class='icons'>
             <div class='dowonload' @click='download'>
-                <img alt='' src='/images/download.png' srcset='' />
+                <svg class='dowonload-img' fill='none' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                    <path d='M19 20H5v2h14v-2zM13 14.5l4.6-4.7 1.5 1.4-7.1 7.1-7.1-7.1 1.4-1.4 4.7 4.7V2h2v12.5z' fill='currentColor' />
+                </svg>
             </div>
             <van-icon class='close' color='#fff' name='cross' size='18' @click='downloadVis=false' />
         </div>
@@ -20,32 +22,25 @@
             <img alt='' src='/images/logo_top.png' srcset='' />
         </div>
         <div class='right'>
-            <div class='user' @click='userVis = true'>
-                <van-icon name='manager' size='20' />
-            </div>
             <div class='menu' @click='menuVis=true'>
-                <van-icon name='wap-nav' size='20' />
+                <svg class='icon-svg' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+                    <use xlink:href='#menu-hamburger-f' />
+                    <symbol id='menu-hamburger-f' viewBox='0 0 24 24'>
+                        <path clip-rule='evenodd' d='M4 4H20V7H4V4ZM4 10.5H20V13.5H4V10.5ZM20 17H4V20H20V17Z' fill='currentColor' fill-rule='evenodd' />
+                    </symbol>
+                </svg>
             </div>
         </div>
     </div>
 
     <van-popup
-        v-model:show='userVis'
-        class='menu-wrap'
-        closeable
-        position='right'
-        :style="{ height: '100%',width: '100%',paddingTop: '60px' }"
-    >
-        <minePerson :data='minePersonData' />
-        <menuList :data='menuListData' />
-    </van-popup>
-
-    <van-popup
         v-model:show='menuVis'
         class='menu-wrap'
+        close-on-popstate
         closeable
         position='right'
-        :style="{ height: '100%',width: '100%',paddingTop: '60px' }"
+        :style="{ height: '100%',width: '100%',paddingTop: '30px' }"
+        teleport='body'
     >
         <div v-if='!customerInfo' class='menu-list'>
             <router-link class='btn' to='/login'>
@@ -55,7 +50,58 @@
                 {{ $t('register.registerBtn') }}
             </router-link>
         </div>
+        <div v-else>
+            <minePerson :data='minePersonData' />
+        </div>
         <van-cell-group class='cellGroup'>
+            <van-cell class='cellItem' is-link :title='$t("cRoute.msg")' @click="routerTo('msg')">
+                <template #icon>
+                    <div class='icon-wrap'>
+                        <svg class='svg-icon' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+                            <use xlink:href='#notification-f' />
+                            <symbol id='notification-f' viewBox='0 0 24 24'>
+                                <path clip-rule='evenodd' d='M12 3C8.13401 3 5 6.13401 5 10L5 13L4 14V16H5L19 16H20L20 14L19 13V10C19 6.13401 15.866 3 12 3ZM12 21C9.94965 21 8.18757 19.7659 7.41602 18H16.5839C15.8124 19.7659 14.0503 21 12 21Z' fill='currentColor' fill-rule='evenodd' />
+                            </symbol>
+                        </svg>
+                    </div>
+                </template>
+            </van-cell>
+            <van-cell class='cellItem' is-link :title='$t("cRoute.regKyc")' @click="routerTo('authentication')">
+                <template #icon>
+                    <div class='icon-wrap'>
+                        <svg class='svg-icon' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+                            <use xlink:href='#id-f' />
+                            <symbol id='id-f' viewBox='0 0 24 24'>
+                                <path clip-rule='evenodd' d='M3 5H21V19H3V5ZM10 10.0003C10 11.1048 9.10457 12.0003 8 12.0003C6.89543 12.0003 6 11.1048 6 10.0003C6 8.8957 6.89543 8.00027 8 8.00027C9.10457 8.00027 10 8.8957 10 10.0003ZM13 9.00027H19V11.5003H13V9.00027ZM19 13.4997H13V15.9997H19V13.4997ZM11 13.4997V15.9997H5V13.4997H11Z' fill='currentColor' fill-rule='evenodd' />
+                            </symbol>
+                        </svg>
+                    </div>
+                </template>
+            </van-cell>
+            <van-cell class='cellItem' is-link :title='$t("cRoute.securitySetting")' @click="routerTo('securitySetting')">
+                <template #icon>
+                    <div class='icon-wrap'>
+                        <svg class='svg-icon' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+                            <use xlink:href='#security-f' />
+                            <symbol id='security-f' viewBox='0 0 24 24'>
+                                <path clip-rule='evenodd' d='M4 4V16L12 21L20 16V4H4ZM12 8L8.5 11.5L12 15L15.5 11.5L12 8Z' fill='currentColor' fill-rule='evenodd' />
+                            </symbol>
+                        </svg>
+                    </div>
+                </template>
+            </van-cell>
+            <van-cell class='cellItem' is-link :title='$t("cRoute.commonSetting")' @click="routerTo('commonSetting')">
+                <template #icon>
+                    <div class='icon-wrap'>
+                        <svg class='svg-icon' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+                            <use xlink:href='#task-center-f' />
+                            <symbol id='task-center-f' viewBox='0 0 24 24'>
+                                <path clip-rule='evenodd' d='M15 2H9V4H5V21H19V4H15V2ZM16 9H8V11.5H16V9ZM16 14.5H8V17H16V14.5Z' fill='currentColor' fill-rule='evenodd' />
+                            </symbol>
+                        </svg>
+                    </div>
+                </template>
+            </van-cell>
             <van-cell class='cellItem' is-link :title='$t("fundInfo.fund")' @click="routerTo('fundProductList')">
                 <template #icon>
                     <div class='icon-wrap'>
@@ -116,7 +162,6 @@ import { changeLang } from '@/api/base'
 import i18n, { loadLocaleMessages } from '@/themeCommon/i18n/i18n.js'
 import { setCookie, getCookie, isEmpty, localGet, localSet, getDevice } from '@/utils/util'
 import Colors, { setRootVariable } from '@plans/colorVariables'
-import menuList from '@plans/modules/menulist/menulist.vue'
 import minePerson from '@plans/modules/minePerson/minePerson.vue'
 
 const store = useStore()
@@ -125,7 +170,6 @@ const route = useRoute()
 
 const downloadVis = ref(true)
 const menuVis = ref(false)
-const userVis = ref(false)
 const menuListData = ref([])
 const minePersonData = ref([])
 
@@ -161,22 +205,22 @@ store.dispatch('_base/getPageConfig', 'Mine').then(res => {
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: rem(26px) 0;
     height: 60px;
+    padding: rem(26px) 0;
     background: rgba(18, 22, 28, 0.9);
     .brand {
         .logo {
-            margin-left: 16px;
-            margin-right: 12px;
             display: inline-block;
-            vertical-align: middle;
-            height: 34px;
             width: 34px;
+            height: 34px;
+            margin-right: 12px;
+            margin-left: 16px;
+            vertical-align: middle;
         }
         .text {
-            vertical-align: middle;
             display: inline-block;
             color: #FFF;
+            vertical-align: middle;
         }
     }
     .icons {
@@ -188,12 +232,12 @@ store.dispatch('_base/getPageConfig', 'Mine').then(res => {
             margin-right: 12px;
             margin-left: 12px;
             padding: 8px rem(10px);
+            text-align: center;
             background-color: #F0B90B;
             border-radius: 50%;
-            text-align: center;
-            img {
-                width: 16px;
-                height: 18px;
+            .dowonload-img {
+                width: 20px;
+                height: 20px;
             }
         }
         .close {
@@ -220,6 +264,10 @@ store.dispatch('_base/getPageConfig', 'Mine').then(res => {
         >div {
             display: inline-block;
             margin-right: rem(30px);
+        }
+        .icon-svg {
+            width: rem(40px);
+            height: rem(40px);
         }
     }
 }
