20487fd19c037740459884af4c2956eb48880502
Moore.h <you@example.com>
2022-03-26 17:26:18 +0800

    底部跳转修改，，wp支持繁体中文

--- src_admin/pages/index/config/lang.js
+++ src_admin/pages/index/config/lang.js
@@ -1,11 +1,17 @@
 export const lang = [
     {
-        name: '中文',
+        name: '简体中文',
         val: 'zh-CN',
         isDefault: true
     },
     {
         name: 'English',
         val: 'en-US'
-    }
+    },
+    {
+        name: '繁體中文',
+        val: 'zh-HK',
+        isDefault: true
+    },
+
 ]
