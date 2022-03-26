de8d36cd139a96ddb2977b1d4223cf0b57c2fd3e
amir.liu <liubin595338764@163.com>
2022-03-26 12:58:22 +0800

    字体大小调整

--- src/themes/plans/layout/layoutTop.vue
+++ src/themes/plans/layout/layoutTop.vue
@@ -427,7 +427,7 @@ const colorSelect = (action) => {
             width: 100%;
             margin-bottom: rem(30px);
             color: rgb(30, 35, 41);
-            font-size: 14px;
+            font-size: 16px;
             text-align: center;
         }
     }
