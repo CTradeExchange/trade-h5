485811fead2bda3b669b0e8479d5b0fed9347244
amir.liu <liubin595338764@163.com>
2022-04-15 17:23:01 +0800

    vitamin配置还原

--- build/h5_environment.js
+++ build/h5_environment.js
@@ -1,11 +1,8 @@
 const uat = []
 
 const pre = [
-    // 'https://h5.aatest.online/',
-    // 'https://pc.aatest.online/'
-    'https://prewph5.cats-trade.com/',
-    'https://prewph5_10.cats-trade.com/',
-    'https://prewppc_10.cats-trade.com/'
+    'https://h5.aatest.online/',
+    'https://pc.aatest.online/'
 ]
 
 module.exports = {
