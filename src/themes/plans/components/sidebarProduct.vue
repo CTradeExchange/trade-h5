fea61eefb5cd2eb33cdf9b1e08bcdfe5e9bf7360
amir.liu <amir.liu@chixi88.com>
2022-04-22 11:44:10 +0800

    滚动条显示优化

--- src/themes/plans/components/sidebarProduct.vue
+++ src/themes/plans/components/sidebarProduct.vue
@@ -176,7 +176,6 @@ export default {
         justify-content: flex-start;
         box-sizing: border-box;
         width: 100%;
-        overflow-y: auto;
         color: var(--color);
         font-weight: 500;
         font-size: rem(30px);
