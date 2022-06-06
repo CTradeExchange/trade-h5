ffc8930c9417ece98447af5bbf2248025502a418
luke <530094021@qq.com>
2022-06-06 14:24:55 +0800

    多语言国旗替换为语言文字

--- src/themeCommon/user/commonSetting.vue
+++ src/themeCommon/user/commonSetting.vue
@@ -66,6 +66,9 @@
                 :class='{ active: lang === item.val }'
                 @click='langSelect(item)'
             >
+                <span class='lang-label'>
+                    {{ item.name }}
+                </span>
                 <img alt='' class='lang-icon' :src="'/images/country_icon/'+ item.val + '.png?555'" />
             </div>
         </div>
@@ -324,21 +327,22 @@ export default {
 }
 .lang-popup {
     .popup-wrap {
-        display: flex;
-        flex-wrap: wrap;
-        padding: 0 0 0 rem(25px);
         .lang-item {
-            box-sizing: content-box;
-            margin-right: rem(20px);
-            margin-bottom: rem(42px);
-            padding: rem(8px) rem(8px) rem(2px) rem(8px);
-            border: rem(4px) solid transparent;
-            .lang-icon {
-                width: rem(120px);
-                height: rem(120px);
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            height: rem(130px);
+            padding: 0 rem(30px);
+            margin-bottom: rem(12px);
+            background: var(--contentColor);
+            border-radius: rem(10px);
+            .lang-label {
+                color: var(--color);
+                font-size: rem(32px);
             }
-            &.active {
-                border: rem(4px) solid var(--primary);
+            .lang-icon {
+                width: rem(72px);
+                height: rem(72px);
                 border-radius: 50%;
             }
         }
@@ -381,7 +385,9 @@ export default {
             }
             .lang-icon {
                 width: rem(56px);
+                height: rem(56px);
                 margin-right: rem(20px);
+                border-radius: 50%;
             }
             .right-arrow {
                 margin-top: -2px;
